5a8b0be2bff54c79eba21590a6773fd433fd716d
Muhammad.Alfalah <Muhammad.Alfalah@hashmicro.com.sg>
2024-02-05 15:52:45 +0800

    first

---
+++ jlox/com/craftinginterpreters/lox/Lox.java
@@ -0,0 +1,57 @@
+package com.craftinginterpreters.lox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Lox{
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPromt();
+        }
+
+    }   
+
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        String x = new String(bytes, Charset.defaultCharset());
+        run(x);
+    }
+
+    private static void runPromt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for(;;){
+            System.out.print("> ");
+            String line = reader.readLine();
+            if(line == null) break;
+            run(line);
+        }
+    }
+
+    private static void run(String source){
+        Scanner scanner = new Scanner(source);
+        List<String> tokens = new ArrayList<>();
+
+        while (scanner.hasNext()) {
+            String token = scanner.next();
+            tokens.add(token);
+        }
+        for(String token : tokens){
+            System.out.println(token);
+        }
+    }
+
+}
