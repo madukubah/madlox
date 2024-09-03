19ad59c6d5b4eb4e86230537bb605383c245a4ad
Muhammad.Alfalah <Muhammad.Alfalah@hashmicro.com.sg>
2024-09-03 14:31:49 +0800

    clox_optimization

--- clox/value.h
+++ clox/value.h
@@ -1,10 +1,53 @@
 #ifndef clox_value_h
 #define clox_value_h
 
+#include <string.h>
+
 #include "common.h"
 
 typedef struct Obj Obj;
 typedef struct ObjString ObjString;
+
+#ifdef NAN_BOXING
+
+#define SIGN_BIT ((uint64_t)0x8000000000000000)
+#define QNAN     ((uint64_t)0x7ffc000000000000)
+
+#define TAG_NIL 1 // 01
+#define TAG_FALSE 2 // 10
+#define TAG_TRUE 3 // 11
+
+typedef uint64_t Value;
+
+#define IS_OBJ(value) (((value) & (QNAN | SIGN_BIT)) == (QNAN | SIGN_BIT))
+#define IS_BOOL(v) ((v) == TRUE_VAL || (v) == FALSE_VAL)
+#define IS_NIL(value) ((value) == NIL_VAL)
+#define IS_NUMBER(value) (((value) & QNAN) != QNAN)
+
+#define AS_OBJ(value) ((Obj*)(uintptr_t)((value) & ~(SIGN_BIT | QNAN)))
+#define AS_BOOL(value) ((value) == TRUE_VAL)
+#define AS_NUMBER(value) valueToNum(value)
+
+#define OBJ_VAL(obj) (Value)(SIGN_BIT | QNAN | (uint64_t)(uintptr_t)(obj))
+#define BOOL_VAL(b) ((b) ? TRUE_VAL : FALSE_VAL)
+#define TRUE_VAL ((Value)(uint64_t) (QNAN | TAG_TRUE))
+#define FALSE_VAL ((Value)(uint64_t) (QNAN | TAG_FALSE))
+#define NIL_VAL ((Value)(uint64_t) (QNAN | TAG_NIL))
+#define NUMBER_VAL(num) numToValue(num)
+
+static inline double valueToNum(Value value){
+    double num;
+    memcpy(&num, &value, sizeof(Value));
+    return num;
+}
+
+static inline Value numToValue(double num){
+    Value value;
+    memcpy(&value, &num, sizeof(double));
+    return value;
+}
+
+#else
 
 typedef enum {
     VAL_BOOL,
@@ -38,6 +81,8 @@ typedef struct {
 #define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
 #define OBJ_VAL(object) ((Value){VAL_OBJ, {.obj = (Obj*)object}})
 
+#endif
+
 typedef struct {
     int count;
     int capacity;
@@ -49,4 +94,5 @@ void writeValueArray(ValueArray* array, Value value);
 void freeValueArray(ValueArray* array);
 void printValue(Value value);
 bool valuesEqual(Value a, Value b);
+
 #endif
